e940acfb2efddc0869a3125645f002fbf659d4c2
Max <mfridm@yahoo.de>
2020-05-27 22:11:12 +0200

    PersonalMovieDB Lection 12

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели','');
+
+
+let personalMovieDB ={
+count: numberOfFilms,
+movies:{},
+actors:{},
+genres:[],
+privat:false
+};
+
+const a = prompt('Последний фильм смотрел какой?',''),
+b = prompt('Какая оценка?',''),
+c = prompt('Последний фильм смотрел какой?',''),
+d = prompt('Какая оценка?','');
+
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
+
