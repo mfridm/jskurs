dca1e480c2fcf30933f40e40b3e85d7adbe96e94
Max <mfridm@yahoo.de>
2020-05-29 14:54:35 +0200

    Lection 15 Циклы и ифы  практика

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 "use strict";
 
-let num=50;
 
-// while(num <= 55) {
-//     console.log(num);
-//     num++;
+let numberOfFilms;
+let personalMovieDB={
+    count:""
+};
+
+do {
+    
+    numberOfFilms = prompt('Какой фильм любимый?','');
+    
+} while(numberOfFilms==null || numberOfFilms=="" || numberOfFilms.length >= 10 );
+
+ personalMovieDB.count = prompt('Сколько фильмов глянул?','');
+ 
+ if(personalMovieDB.count<10){
+     alert("ну ты слабак");
+ }else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
+     alert("ну норм ты мужик");
+ }else{
+     alert("ты наркоман");
+ }
+
+// while(numberOfFilms != '' && numberOfFilms.length =50) {
+//     console.log(str.length);
 // }
 
 // do {
-//     console.log(num);
-//     num++;
-// }
-// while (num<=55);
-
-for (let i=1; i<8; i++){
-    if(i===6){
-        // break;
-        continue;
-    }
-    console.log(i);
+//     console.log(`${str.lenght}`);
     
-}
+// }
+// while (numberOfFilms>=50);
+
+// for (let i=1; i<0; i++){
+//     if(numberOfFilms){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
