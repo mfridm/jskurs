11e47802e5ed0dcc67e8e650ea4d85f0508c2f3d
Max <mfridm@yahoo.de>
2020-05-31 23:16:24 +0200

    Lection 19 колбеки

--- js/script.js
+++ js/script.js
@@ -1,67 +1,29 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели','');
+function first(){
+    //вывод с задержкой
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
 
 
-let personalMovieDB ={
-count: numberOfFilms,
-movies:{},
-actors:{},
-genres:[],
-privat:false
-};
 
-const a = prompt('Последний фильм смотрел какой?',''),
-b = prompt('Какая оценка?',''),
-c = prompt('Последний фильм смотрел какой?',''),
-d = prompt('Какая оценка?','');
+function second(){
+    console.log(2);
+}
 
+first();
+second();
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+function learnJS(lang, callback){
+    console.log(`я учу: ${lang}`);
+    callback();
+}
 
-function showMyDB(){
-   if(personalMovieDB.privat==false) {
-    console.log(personalMovieDB);
-   }else{
-    console.log("Huj Tam TRUE");
-   }
+function done(){
     
+    console.log('я прошел этот урок');
 }
 
-showMyDB();
-
-let arr=[];
-let MyQuestion;
-function writeYourGenres(){
-    for(let i=1;i<4;i++){
-        MyQuestion=prompt(`укажи свой любимый жанр ${i}`,'');
-        arr.push(MyQuestion);
-    }
-    console.log(arr);
-}
-
-writeYourGenres();
-
-// function calc(a,b){
-//     return(a+b);
-// }
-
-// console.log(calc(4,3));
-// console.log(calc(5,6));
-// console.log(calc(10,6));
-
-// function ret(){
-//     let num=50;
-//     return num;
-// }
-
-// const anotherNum= ret();
-// console.log(anotherNum);
-
-// const logger = function(){
-// console.log("Hello")
-// };
-// logger();
-
-// const calc=(a,b)=>a+b;
+learnJS('JavaScript', done); // передаем 1 аргумент текстом, второй всю функцию
