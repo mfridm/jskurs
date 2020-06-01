f6d25dce6bf0c184e864ff2087d6ecc2e3329f76
Max <mfridm@yahoo.de>
2020-06-01 22:35:20 +0200

    Lection 21 массивы,псевдомассивы,сортировка-2

--- js/script.js
+++ js/script.js
@@ -1,44 +1,5 @@
 "use strict";
 
-<<<<<<< 5710852d928f6691d7ac3ef4f3594f6b64ace58b
-const options= {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors:{
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function(){
-        console.log("Test");
-    }
-};
-
-options.makeTest();
-
-const {border, background} = options.colors;
-console.log(options.colors);
-
-// console.log(Object.keys(options).length);
-
-// console.log(options['colors']['border']);
-
-//delete options.name; //удаление ключа
-
-// console.log(options);
-// let counter =0;
-// for(let key in options){
-//     if(typeof(options[key]) === 'object'){ //проверяем есть ли объекты в объекте
-//         for (let i in options[key]){ //если есть объекты внутри то выводим все их содержимое
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//             counter++; // два уровня погружения. ки обращается к ключам подряд, и обращается ко второму уровню глубины
-//         }
-//     }else{
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
-    
-=======
 const arr=[2,13,26,8,10];
 arr.sort(compareNum);
 console.log(arr);
@@ -62,8 +23,16 @@ function compareNum(a,b){
 
 // for (let i=0;i<arr.length;i++){
 //     console.log(arr[i]);
->>>>>>> Lection 21 массивы,псевдомассивы,сортировка
 // }
-// console.log(counter);
+
+// for (let value of arr){
+//     console.log(value);// валью выводит значения аррея
+// }
+
+const str = prompt("",""); // принимаем любой формат
+const products = str.split(","); // записываем в переменную разделяя запятой
+products.sort();
+console.log(products.join('; ')); //склеиваем в одну строку, разделяя семиколоном
+
 
 
