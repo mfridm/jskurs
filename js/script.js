0e7c76a9aa647310d7d7d617253b2704da8c407a
Max <mfridm@yahoo.de>
2020-06-02 22:29:39 +0200

    Lection 22 копия объектов и массивов, спред оператор

--- js/script.js
+++ js/script.js
@@ -1,38 +1,86 @@
 "use strict";
 
-const arr=[2,13,26,8,10];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5,
+//     b = a;
 
-function compareNum(a,b){
-    return a-b;
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // всего лишь ссылка на объект. содержимое не копируется
+// copy.a = 10; // т.к. копи изначально ссылка то а=10 в обж
+
+//console.log(copy);
+//console.log(obj);
+
+function copy(mainObj){ // поверхностная копия
+    let objCopy = {};
+    let key;
+    for (key in mainObj){
+        objCopy[key]=mainObj[key];
+    }
+    return objCopy;
 }
-// arr[99]=0;
-// console.log(arr.length);
-// console.log(arr);
 
-// ФорИч переберает все в аррее. аргументы в функции любые.
-// первый это содержимое, второй индекс, третий имя массива
-// arr.forEach(function(item, i, arr){
-//     console.log(`${i}: ${item} vnutri massiva ${arr}`);
-// });
+const numbers = {
+    a:2,
+    b:5,
+    c:{
+        x:7,
+        y:4
+    }
+};
 
-// arr.pop();
-// arr.push(10);
-// console.log(arr);
+const newNumbers = copy(numbers);
+newNumbers.a=10;
+newNumbers.c.x=10;
 
-// for (let i=0;i<arr.length;i++){
-//     console.log(arr[i]);
-// }
+// console.log(newNumbers);
+// console.log(numbers);
 
-// for (let value of arr){
-//     console.log(value);// валью выводит значения аррея
-// }
+const add = {
+    d: 17,
+    e: 20
+}
 
-const str = prompt("",""); // принимаем любой формат
-const products = str.split(","); // записываем в переменную разделяя запятой
-products.sort();
-console.log(products.join('; ')); //склеиваем в одну строку, разделяя семиколоном
+console.log(Object.assign(numbers, add)); //добавляем методом один объект в другой
 
+const clone = Object.assign({},add);//создаем пустой объект и лепим к нему адд
+clone.d = 20;
+// console.log(clone);
 
+const oldArray = ['a','b','c'];
+const newArray = oldArray.slice(); // метод нарезки массива и копии
+newArray[0]='ASDASDASD'; // добавляем значение на первое место
+console.log(newArray);
+console.log(oldArray);
 
+const video=['youtube','vimeo','rutube'], 
+    blogs=['wordpress', 'livejournal', 'blogger'],
+    internet=[...video, ...blogs, 'vk', 'fb']; //троеточие спред, добавляет к массиву другой массив
+
+console.log(internet);
+
+function log(a,b,c){ // сюда можно передать 3 отдельных значения
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2,5,7]; // массив из трех значений
+log(...num); // спредов в функцию лог, через запятую передали массив
+
+const array = ["a","b"];
+const newAarray =[...array];
+const q = {
+    one:1,
+    two:2
+};
+const newObjects ={...q}; //копируем объект любой при помощи спред...
+console.log(newObjects);
