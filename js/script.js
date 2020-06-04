5bcfab499a321169bfdb6384fd3e408c8f7f8385
Max <mfridm@yahoo.de>
2020-06-04 21:48:06 +0200

    Lection 26 преобразования типов данных

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
 "use strict";
 
-// let str = "some";
-// let strObj = new String(str);
+//1)
+    console.log(typeof(String(null)));
+    console.log(typeof(String(4)));
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
+//2)
+console.log(typeof((5+''))); // при сложение со строкой всегда строка тип данных
 
-// console.dir([1,2,3]);
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
+const fontSize=26 + 'px';
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello:function(){
-        console.log("Hello");
-    }
-};
+// To Number
 
-const john = Object.create(soldier); // создание протопиной связи с солдиер сразу при создании переменной
+//1)
 
+console.log(typeof(Number('4')));
 
-// Object.setPrototypeOf(john, soldier); // создание протопиной связи с солдиер
+//2)
+console.log(typeof(+'5'));
 
-john.sayHello();
+//3)
+console.log(typeof(parseInt("15px", 10)));
+
+let answer = +prompt("Hello","");
+
+// to boolean
+// 0,'', null, undefined,NaN; // это все фолс
+1)
+let switcher = null;
+if(switcher){
+    console.log('working...');
+}
+switcher = 1;
+if(switcher){
+    console.log('working...');
+}
+2)
+console.log(typeof(Boolean('4')));
+
+3)
+console.log(typeof(!!"444")); // превратили в булеан
